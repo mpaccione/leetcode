2bd93d8a148935396537556f16897422c887a824
mpaccione <mpaccione1991@gmail.com>
2021-03-16 17:05:28 -0700

    firstBadVersion

--- easy/sortingSearching/firstBadVersion.js
+++ easy/sortingSearching/firstBadVersion.js
@@ -2,8 +2,6 @@
 // INSTRUCTIONS //
 //////////////////
 
-const { first } = require("underscore");
-
 // You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
 // Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
@@ -23,25 +21,22 @@ function isBadVersion(version) {
 
 const isFirstBadVersion = (startVersion, endVersion) => {
   if (endVersion - startVersion <= 1) {
-    console.log("END")
     return isBadVersion(endVersion) ? endVersion : startVersion;
   }
 
   // Implement Recursive Binary Search
   const midVersion = parseInt((endVersion - startVersion) / 2) + startVersion;
-  console.log(`${startVersion} - ${midVersion} - ${endVersion}`);
-
-  isBadVersion(midVersion)
+  // console.log(`${startVersion} - ${midVersion} - ${endVersion}`);
+  return isBadVersion(midVersion)
     ? isFirstBadVersion(startVersion, midVersion)
     : isFirstBadVersion(midVersion, endVersion);
 };
 
-console.log(isFirstBadVersion(0, latestVersion));
 
 module.exports = function () {
-  describe("Check for Sorted Merge", () => {
-    it("Array values are merged and sorted", () => {
-      expect(nums1.concat(nums2).sort()).toEqual(mergeArray(nums1, nums2));
+  describe("Check for Bad Version match", () => {
+    it("Version match", () => {
+      expect(isFirstBadVersion(0, latestVersion)).toEqual(61);
     });
   });
 };
--- easy/sortingSearching/index.js
+++ easy/sortingSearching/index.js
@@ -1,9 +1,12 @@
 const mergeSortedArray = require("./mergeSortedArray.js")
+const firstBadVersion = require("./firstBadVersion.js")
 
 // Sorting and Searching
 mergeSortedArray()
+firstBadVersion()
 
 
 module.exports = function(){
     mergeSortedArray()
+    firstBadVersion()
 }
