997f1f1cf2ba08da35dc67dbd7ce6ef5c2038e99
mpaccione <mpaccione1991@gmail.com>
2021-03-26 11:16:50 -0700

    reverseInteger

--- easy/string/index.js
+++ easy/string/index.js
@@ -1,16 +1,19 @@
 const reverseString = require("./reverseString.js")
+const reverseInteger = require("./reverseInteger.js")
 const firstUniqueCharInString = require("./firstUniqueCharInString.js")
 const validAnagram = require("./validAnagram.js")
 const validPalindrome = require("./validPalindrome.js")
 
 // Strings
 // reverseString()
+// reverseInteger()
 // firstUniqueCharInString()
 // validAnagram();
 // validPalindrome();
 
 module.exports = function(){
     reverseString();
+    reverseInteger();
     firstUniqueCharInString();
     validAnagram();
     validPalindrome();
---
+++ easy/string/reverseInteger.js
@@ -0,0 +1,51 @@
+//////////////////
+// INSTRUCTIONS //
+//////////////////
+
+// Given a signed 32-bit integer x, return x with its digits reversed.
+// If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+// Input: x = 123
+// Output: 321
+
+// Input: x = -123
+// Output: -321
+
+// Input: x = 120
+// Output: 21
+
+// Input: x = 0
+// Output: 0
+
+const input1 = 123;
+const input2 = -123;
+const input3 = 120;
+const input4 = 0;
+
+const reverseInteger = (num) => {
+  const reversed = parseInt(num.toString().split("").reverse().join(""));
+  console.log({ reversed });
+  return reversed >= -Math.pow(2, 31) && reversed <= Math.pow(2, 31) - 1
+    ? num >= 0
+      ? reversed
+      : -reversed
+    : 0;
+};
+
+module.exports = function () {
+  describe("Check for Reversal", () => {
+    it("Use Case 1", () => {
+      expect(reverseInteger(input1)).toEqual(321);
+    });
+    it("Use Case 2", () => {
+      expect(reverseInteger(input2)).toEqual(-321);
+    });
+    it("Use Case 3", () => {
+      expect(reverseInteger(input3)).toEqual(21);
+    });
+    it("Use Case 4", () => {
+      expect(reverseInteger(input4)).toEqual(0);
+    });
+  });
+};
